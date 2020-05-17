02a46c3a622a5f503a10eb8fbcb8cd843447e759
mostafa <mostafakhoshhal@yahoo.com>
2020-05-17 18:12:07 +0430

    edited version

--- digikala_apple.py
+++ digikala_apple.py
@@ -1,14 +1,17 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import csv
 
-response = requests.get('https://www.digikala.com/brand/apple/') 
+response = requests.get('https://www.digikala.com/brand/apple/?has_selling_stock=1&pageno=1&sortby=4') 
 soup = BeautifulSoup(response.text,'html.parser')
 
-data = soup('div', attrs={'class':'c-product-box__content'})
+data = soup.find_all('div', attrs={'class':'c-product-box__content'})
 
-f = open('apple.txt','w+')
+f = open('apple.csv','w+')
+
 for stuff in data:
-    print(stuff.text)
-    print()
-    f.write(stuff.text)
+    f.write(re.sub(r'\s+',' ',stuff.text))
+    f.write('\n')
+    
+f.close()
